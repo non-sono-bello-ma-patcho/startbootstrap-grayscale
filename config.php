0e8385a37b2bd2737c707803a90a6a2e5cbfa486
rollingflamingo <andrea.storace.uni@gmail.com>
2019-01-13 16:35:44 +0100

    added configuration file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phibonachos
+ * Date: 13/01/19
+ * Time: 16.34
+ */
+
+return array(
+    'index' => 'index.html',
+    'private' => 'private.html',
+);
