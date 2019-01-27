e45f2fb6284cafd656af680a10398921e603590b
rollingflamingo <andrea.storace.uni@gmail.com>
2019-01-27 17:58:29 +0100

    modified modify page css

--- js/modify.js
+++ js/modify.js
@@ -31,3 +31,11 @@ function displayPreview(input){
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+$('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+});
+
+$('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+});
--- modifyform.php
+++ modifyform.php
@@ -15,71 +15,80 @@
     <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="css/modify.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/grayscale.css">
 </head>
-<body class="bg-light">
-<div class="container">
+<body class="">
+<div class="modifypage">
     <div class="py-5 text-center">
         <img class="d-block mx-auto mb-4" src="img/logo_purple_char.png" alt="">
         <h2>Modify Profile Informations</h2>
         <p class="lead">Make sure to set all the fields you want to change</p>
-        <div class="row">
-            <div class="col-md-4">
-                <h3 class="text-left text-muted mb-5">User image</h3>
-                <div class="card">
-                    <div class="card-header">
-                        <img id="preview" src="img/default-account.png" class="card-img-top custom-userimage mx-auto mt-2" alt="..." style="overflow: hidden">
-                    </div>
-                    <div class="card-body">
-                        <form action="">
-                            <div class="input-group mb-3 my-auto">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Upload</span>
+        <div class="container mx-auto">
+            <div class="row">
+                <div class="col-md-3" style="background-color: rgba(0,0,0,0.2); border-top-left-radius: calc(0.25rem - 1px); border-bottom-left-radius: calc(0.25rem - 1px)">
+                    <ul class="nav flex-column">
+                        <li class="dropdown-item">
+                            <a href="" class="nav-link active text-muted">User Informations</a>
+                        </li>
+                        <li class="dropdown-item">
+                            <a href="" class="nav-link text-muted">Email & Billing</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-9" style="background-color: rgba(0,0,0,0.2); border-top-right-radius: calc(0.25rem - 1px); border-bottom-right-radius: calc(0.25rem - 1px)">
+                    <h3 class="text-left text-muted my-3">User Information</h3>
+                    <form action="">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-row">
+                                        <label for="email" class="text-muted">Email</label>
+                                        <input type="email" class="form-control mb-3" id="email" aria-describedby="emailinput" placeholder="change your email...">
+                                    </div>
+                                    <div class="form-row">
+                                        <label for="username" class="text-muted">Username</label>
+                                        <input type="email" class="form-control mb-3" id="username" aria-describedby="emailinput" placeholder="change your username...">
+                                    </div>
+                                    <div class="form-row">
+                                        <label for="email" class="text-muted">Name</label>
+                                        <input type="email" class="form-control mb-3" id="name" aria-describedby="nameinput" placeholder="change your name...">
+                                    </div>
                                 </div>
-                                <div class="custom-file text-left">
-                                    <input type="file" class="custom-file-input" id="inputGroupFile01" onchange="loadname(); displayPreview(this)">
-                                    <label class="custom-file-label" for="inputGroupFile01" id="inputGroupFile01label">Choose file</label>
+                                <div class="col-md-4">
+                                    <div class="form-row">
+                                        <label for="username" class="text-muted">Surname</label>
+                                        <input type="email" class="form-control mb-3" id="surname" aria-describedby="surnameinput" placeholder="change your surname...">
+                                    </div>
+                                    <div class="form-row">
+                                        <label for="email" class="text-muted">Location</label>
+                                        <input type="email" class="form-control mb-3" id="name" aria-describedby="nameinput" placeholder="change your location...">
+                                    </div>
+                                    <div class="form-row">
+                                        <label for="email" class="text-muted">Description</label>
+                                        <textarea class="form-control" aria-label="With textarea"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 text-center">
+                                    <a href="" class="mx-auto" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <img id="preview" src="img/default-account.png" class="custom-userimage mt-2" alt="..." style="overflow: hidden">
+                                    </a>
+                                    <div class="dropdown-menu">
+                                        <a href="" class="dropdown-item">Choose image</a>
+                                        <a href="" class="dropdown-item">Delete image</a>
+                                    </div>
                                 </div>
                             </div>
-                        </form>
+                        </div>
+                    </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-8">
-                <h3 class="text-left text-muted mb-5">User Information</h3>
-                <form action="">
-                    <div class="form-row">
-                        <div class="col-md-6 text-left">
-                            <label for="email" class="text-muted">Email</label>
-                            <input type="email" class="form-control mb-3" id="email" aria-describedby="emailinput" placeholder="change your email...">
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label for="username" class="text-muted">Username</label>
-                            <input type="email" class="form-control mb-3" id="username" aria-describedby="emailinput" placeholder="change your username...">
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="col-md-6 text-left">
-                            <label for="email" class="text-muted">Name</label>
-                            <input type="email" class="form-control mb-3" id="name" aria-describedby="nameinput" placeholder="change your name...">
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label for="username" class="text-muted">Surname</label>
-                            <input type="email" class="form-control mb-3" id="surname" aria-describedby="surnameinput" placeholder="change your surname...">
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="col-md-6 text-left">
-                            <label for="email" class="text-muted">Location</label>
-                            <input type="email" class="form-control mb-3" id="name" aria-describedby="nameinput" placeholder="change your location...">
-                        </div>
-                    </div>
-                </form>
             </div>
         </div>
     </div>
 </div>
 <!-- Footer -->
-<footer class="bg-black small text-center text-white-50 fixed-bottom">
+<footer class="bg-black small text-center text-white-50">
     <div class="container">
         Copyright &copy; Herschel 2018
     </div>
