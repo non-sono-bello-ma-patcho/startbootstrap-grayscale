ac22c8ef8c0ce00b661b4bdbfbd9fc2617b1fd7d
rollingflamingo <andrea.storace.uni@gmail.com>
2019-02-02 15:22:59 +0100

    added redirect page

---
+++ redirect_wrong_credential.php
@@ -0,0 +1,33 @@
+<?php session_start();
+require 'php/userUtility.php';
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Herschel - Mismatched Credentials</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/redirect.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/grayscale.css">
+</head>
+<body class="">
+    <div class="">
+    </div>
+    <!-- Footer -->
+    <footer class="bg-black small text-center text-white-50">
+    <div class="container">
+        Copyright &copy; Herschel 2018
+    </div>
+</footer>
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.js"></script>
+<script src="js/redirect.js"></script>
+</body>
+</html>
