af585c473272279a78d11b8db9f9f2f9653424f2
Alessandro <alex.savona97@gmail.com>
2019-03-16 15:03:07 +0100

    merge

--- js/private.js
+++ js/private.js
@@ -4,3 +4,86 @@ function doLogout() {
     window.location.href = '../php/logout.php';
     console.log("destroyed");
 }
+
+function searchUserbyUsername(){
+    $('#resultlist').empty();
+    username = $('#newusername').val();
+    // return a list? of user with name similar to given
+    console.log("got filter: "+username);
+    $.post("php/formUtility.php", { username : username, op : "searchuser" },function(response){
+        userinfo = JSON.parse(response);
+        if(userinfo.length <= 0){
+            $('#adminufb').text(`${username} doesn't match any profile...`);
+            $('#resultlist').toggleClass('d-none', true).toggleClass("custom-hidden", true);
+            $('#newusername').toggleClass('is-invalid');
+        }
+        else {
+            $('#newusername').toggleClass('is-invalid', false);
+            console.log("Found matching users: "+userinfo.length);
+            items = userinfo.length;
+            offset = 0;
+            maxoffset = Math.floor(items/3)*3;
+            decks = Math.ceil(items/3);
+            console.log("Initializing "+decks+" decks");
+            for(i=0; i<decks; i++){
+                deck = $("<div class=\"card-deck\"></div>");
+                console.log("maxoffset: "+maxoffset+" as module value is: "+3%items);
+                console.log("Cycling over"+(offset<=maxoffset?items-maxoffset:3));
+                for(j=0; j<(offset===maxoffset?items-maxoffset:3); j++){
+                    console.log("index is: "+(offset+j));
+                    deck.append(`<div class="card">
+    <div class="card-header">
+        <div class="form-check">
+            <input type="radio" class="form-check-input" name="usernamec">
+            <label for="usernamec" class="form-check-label">${userinfo[j+offset].username}</label>
+        </div>
+        </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-6">
+                <p id="nametag">${userinfo[j+offset].name}</p>
+            </div>
+            <div class="col-md-6">
+                <p id="surnametag">${userinfo[j+offset].surname}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p class="mb-0" id="emailtag">${userinfo[j+offset].email}</p>
+            </div>
+        </div>
+    </div>
+</div>`);
+                }
+                offset += 3;
+                $('#resultlist').append(deck);
+            }
+            $('#resultlist').toggleClass('d-none', false).toggleClass("custom-hidden", false);
+        }
+    });
+}
+
+/* Card sample:
+<div class="card" style="max-width: 18rem;">
+    <div class="card-header">
+        <div class="form-check">
+            <input type="radio" class="form-check-input" name="usernamec">
+            <label for="usernamec" class="form-check-label">phibonachos</label>
+        </div>
+        </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-4">
+                <p id="nametag">Mario</p>
+            </div>
+            <div class="col-md-4">
+                <p id="surnametag">Rossi</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p class="mb-0" id="emailtag">example@example.it</p>
+            </div>
+        </div>
+    </div>
+</div>*/
