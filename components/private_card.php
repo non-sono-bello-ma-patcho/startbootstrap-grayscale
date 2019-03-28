0a924920467d758fa3b2cc9f20ae54e54f405f8e
rollingflamingo <andrea.storace.uni@gmail.com>
2019-03-29 00:53:24 +0100

    fixed wrong path for imgs

--- components/private_card.php
+++ components/private_card.php
@@ -6,16 +6,15 @@
  * Time: 23.14
  */
 
-$product_link = 'herschel.hopto.org/products.php?id=';
 // a seconda che la carta sia vista nello store, nel carrello o wishlist, deve reindirizzare sul carrello o sul dettaglio
-isset($_REQUEST['name']) ? $card_title = trim($_REQUEST['name']) : $card_title = $name;
-isset($_REQUEST['description']) ? $card_description = trim($_REQUEST['description']) : $card_description = $description;
-isset($_REQUEST['price']) ? $card_price = trim($_REQUEST['price']) : $card_price = $price;
-isset($_REQUEST['img']) ? $card_image = 'img/productImg/'.trim($_REQUEST['img']) : $card_image = $img;
-isset($_REQUEST['code']) ? $product_link .= trim($_REQUEST['code']) : $product_link .= $code;
+$card_title =  isset($_REQUEST['name']) ? trim($_REQUEST['name']) : $card_title = $name;
+$card_description = isset($_REQUEST['description']) ? trim($_REQUEST['description']) : $card_description = $description;
+$card_price = isset($_REQUEST['price']) ? trim($_REQUEST['price']) : $card_price = $price;
+$card_image = isset($_REQUEST['img']) ? trim($_REQUEST['img']) : $card_image = $img;
+$product_link = 'herschel.hopto.org/products.php?id='.(isset($_REQUEST['code']) ? trim($_REQUEST['code']) :  $code);
 
 echo "
-<div class=\"col-sm-6 position-relative mt-3 pr-0\">
+<div class=\"col-lg-6 position-relative mt-3 pr-0\">
     <div class=\"custom-card card slim-card text-white text-left mt-2 mb-3\">
         <a href=\"{$product_link}\" class=\"read-more text-white\">Read More</a>
         <img src=\"{$card_image}\" class=\"card-img\" alt=\"...\">
