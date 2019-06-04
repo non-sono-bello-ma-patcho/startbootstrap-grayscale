90f9130c4f8db15cd75d20be1d3517eb193f4ba9
Andrea Storace <a.storace@wikicasa.it>
2019-06-04 15:39:53 +0200

    minor fix
    added navifation bar to modify page
    removed outline from buttons

--- WebSrc/assets/pages/modifyform.php
+++ WebSrc/assets/pages/modifyform.php
@@ -24,8 +24,31 @@ $img = getUserImg($username);
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Modify User Information</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+
 </head>
 <body >
+
+<nav class="navbar navbar-expand-lg navbar-shrink navbar-light shadow" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand a-logo" href="index.php"></a>
+        <h1 class="mx-auto my-0 text-uppercase gradient-title">Herschel</h1>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="" data-toggle="modal" data-target="#logoutModal">Log Out</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <div class="form-container mx-auto card p-2">
         <div class="row text-left justify-content-center">
             <h4 class="text-primary text-center w-100">Edit your profile</h4>
